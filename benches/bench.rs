c78243beddaea2961b2bb6ce257ae6edc4ae1c37
Frank Harkins <frankharkins@hotmail.co.uk>
2026-02-07 13:08:41 +0000

    Add benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,24 +1,41 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use itertools::Itertools;
 use std::fs;
 
-fn readme(c: &mut Criterion) {
-    let doc = fs::read_to_string("readme.md").unwrap();
-
-    c.bench_with_input(BenchmarkId::new("readme", "readme"), &doc, |b, s| {
+fn tiny_markdown_string(c: &mut Criterion) {
+    let doc = "A *single* [markdown](/path) string!".to_owned();
+    c.bench_with_input(BenchmarkId::new("tiny", "markdown_string"), &doc, |b, s| {
         b.iter(|| markdown::to_html(s));
     });
 }
 
-// fn one_and_a_half_mb(c: &mut Criterion) {
-//     let doc = fs::read_to_string("../a-dump-of-markdown/markdown.md").unwrap();
-//     let mut group = c.benchmark_group("giant");
-//     group.sample_size(10);
-//     group.bench_with_input(BenchmarkId::new("giant", "1.5 mb"), &doc, |b, s| {
-//         b.iter(|| markdown::to_html(s));
-//     });
-//     group.finish();
-// }
-// , one_and_a_half_mb
+fn readme(c: &mut Criterion) {
+    let doc = fs::read_to_string("readme.md").unwrap();
 
-criterion_group!(benches, readme);
+    c.bench_with_input(BenchmarkId::new("medium", "readme"), &doc, |b, s| {
+        b.iter(|| markdown::to_html(s));
+    });
+}
+
+fn large_jsx_expressions(c: &mut Criterion) {
+    let num_components = 3;
+    let num_jsx_lines_per_component = 5000;
+    fn dummy_component(code: String) -> String {
+        return format!("<DummyComponent code={{`{code}`}} />");
+    }
+    fn uuids(len: usize) -> String {
+        (0..len)
+            .map(|_| "770f93e8-b4ee-4ce8-ab0f-4ece7d8c1090")
+            .join("\n")
+    }
+    let doc = (0..num_components)
+        .map(|_| dummy_component(uuids(num_jsx_lines_per_component)))
+        .join("\n\n");
+
+    c.bench_with_input(BenchmarkId::new("large", "jsx_expression"), &doc, |b, s| {
+        b.iter(|| markdown::to_html(s));
+    });
+}
+
+criterion_group!(benches, tiny_markdown_string, readme, large_jsx_expressions);
 criterion_main!(benches);
